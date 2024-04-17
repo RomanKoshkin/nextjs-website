ded3ccabea676bef66fb73f211cee2bec36d97f8
Roman Koshkin <romankoshkin@Romans-MacBook-Pro.local>
2024-04-17 15:32:26 +0900

    CSP

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const headers = [
 	{
 		key: 'Content-Security-Policy',
 		value:
-			"default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'self' https://www.googletagmanager.com https://www.google-analytics.com; img-src 'self' data:; connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
+			"default-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'self' https://www.googletagmanager.com https://www.google-analytics.com; img-src 'self' data:; connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com",
 	},
 	{ key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
 	{ key: 'X-Frame-Options', value: 'DENY' },
