527bd10f646d9181c14238c6038573e5dcfd7db2
Roman Koshkin <romankoshkin@Romans-MacBook-Pro.local>
2026-05-07 11:29:15 +0900

    disable redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,7 @@
 import Redis from 'ioredis'
 
-export const redis = new Redis({
+export const redis = process.env.USE_REDIS === 'true' ? new Redis({
     host: process.env.REDIS_URL,
-    port: parseInt(process.env.REDIS_PORT || '6379'), // Provide a default value for REDIS_PORT
+    port: parseInt(process.env.REDIS_PORT || '6379'),
     password: process.env.REDIS_PASSWORD
-})
+}) : null
--- src/lib/views.ts
+++ src/lib/views.ts
@@ -1,12 +1,14 @@
 import { redis } from 'lib/redis'
 
 export async function getViews(slug: string): Promise<string> {
+	if (!redis) return '1'
 	const count = await redis.hget('views', slug)
 	console.log(count);
 	return count !== null ? count : '1'
 }
 
 export async function increseViews(slug: string) {
+	if (!redis) return
 	console.log(slug);
 	const count = await redis.hget('views', slug)
 
